3ef40fd77628d9b99572d7dc457938a00f80ce2d
atararushi001 <atararushi111111@gmail.com>
2024-03-15 23:14:10 +0530

    CompanyDirectory

---
+++ api/controllers/companyDirectory.php
@@ -0,0 +1,37 @@
+<?php
+include "../includes/config.php";
+
+function handleError($message)
+{
+    http_response_code(500);
+    echo json_encode(array('success' => false, 'message' => $message));
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    $query = "SELECT * FROM `recruiters`";
+    $stmt = $conn->prepare($query);
+
+    try {
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        $rows = $result->fetch_all(MYSQLI_ASSOC);
+
+ 
+
+        $response = array(
+            'success' => true,
+            'message' => 'company found!',
+            'job' => $rows,
+        );
+
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    } catch (Exception $e) {
+        handleError("Database error: " . $e->getMessage());
+    } finally {
+        $stmt->close();
+        $conn->close();
+    }
+}
