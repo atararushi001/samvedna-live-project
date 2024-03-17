9a0030ca320be7efb088b1f5b7948e3d4ad6a2cd
atararushi001 <atararushi111111@gmail.com>
2024-03-17 21:26:42 +0530

    testdata

--- api/includes/config.php
+++ api/includes/config.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin: https://mysamvedna.org");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, multipart/form-data, application/json");
 header("Access-Control-Allow-Credentials: true");
