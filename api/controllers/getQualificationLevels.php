4caac14aca0970455e08ada42d798a4dd20435c2
ThankiAshish <thankiashish555@gmail.com>
2024-03-18 12:47:12 +0530

    Bug Fixes

--- api/controllers/getQualificationLevels.php
+++ api/controllers/getQualificationLevels.php
@@ -5,7 +5,6 @@ include '../includes/config.php';
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
     $data = mysqli_query($conn, "SELECT * FROM qualificationlevel");
-    echo '<option value="">Select Qualification Level</option>';
     while ($datarow = mysqli_fetch_array($data)) {
         echo '<option value="' . $datarow['qualification_id'] . '">' . $datarow['qualification_name'] . '</option>';
     }
